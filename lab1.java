27e52e523aee18d51463bfa7962b80932701d88b
pkokkinos <kokkinop@gmail.com>
2020-10-06 11:41:50 +0300

    Create lab1.java

---
+++ lab1.java
@@ -0,0 +1,12 @@
+public class Age
+{
+  public static void main(String args[])
+  {
+    if (args.length == 1)
+    {
+      System.out.println("You are ");
+      System.out.println(args[0]);
+      System.out.println(" years old.");
+    }
+  }
+}
