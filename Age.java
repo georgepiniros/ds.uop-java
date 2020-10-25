a9e10affbae66f5999cd387ddbf78c841c2b0857
george <carmania@hotmail.gr>
2020-10-25 23:21:38 +0200

    2018098

--- Age.java
+++ Age.java
@@ -1,12 +1,20 @@
-public class Age
-{
-  public static void main(String args[])
-  {
-    if (args.length == 1)
-    {
-      System.out.print("You are ");
-      System.out.print(args[0]);
-      System.out.println(" years old.");
+public class Age {
+  public static void main(String args[]) {
+    try {
+      if (args.length == 1) {
+        System.out.print("You are ");
+        System.out.print(args[0]);
+        System.out.println(" years old.");
+      }
+    }catch(Exception e){
+
+        System.out.printf("My AM is %s", args[1]);
+      }
+
+
     }
   }
-}
+
+
+
+
